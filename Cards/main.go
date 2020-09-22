01fbeb69a5e2ed30a3c2a31af9e4faa44062b6d7
Shivasubramaniamnr <subramanium.shiva@gmail.com>
2020-09-23 00:33:45 +0530

    commit

--- Cards/main.go
+++ Cards/main.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Ace of Spades"
+	card := NewCard()
+
 	fmt.Println(card)
+}
+
+func NewCard() string {
+
+	return "five of diamonds"
 }
