3be14cec94acef6f43d6aae25268bcaa11c9faf3
Shivasubramaniamnr <subramanium.shiva@gmail.com>
2020-10-03 00:01:02 +0530

    commiy

--- Cards/main.go
+++ Cards/main.go
@@ -4,7 +4,11 @@ import "fmt"
 
 func main() {
 	var strname string
+
 	fmt.Println("Please enter 2 Letter state name")
+
 	fmt.Scanln(&strname)
+
+	// stateName func is to find the abbreviation of 2 letter state code
 	fmt.Println(stateName(strname))
 }
