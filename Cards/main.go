675fd93b4a428b045767225cd1c5811fa24a3baa
Shivasubramaniamnr <subramanium.shiva@gmail.com>
2020-10-03 00:32:28 +0530

    commit

--- Cards/main.go
+++ Cards/main.go
@@ -5,10 +5,15 @@ import "fmt"
 func main() {
 	var strname string
 
-	fmt.Println("Please enter 2 Letter state name")
+	for k := 0; k <= 10; k++ {
+		fmt.Println("Please enter 2 Letter state name, to stop enter STOP")
 
-	fmt.Scanln(&strname)
+		fmt.Scanln(&strname)
 
-	// stateName func is to find the abbreviation of 2 letter state code
-	fmt.Println(cityName(strname))
+		if strname == "STOP" {
+			break
+		}
+		// stateName func is to find the abbreviation of 2 letter state code
+		fmt.Println(cityName(strname))
+	}
 }
